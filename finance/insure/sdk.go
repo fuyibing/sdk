31fddc56b465368c6478ff17405270b53cd98acc
Darrow <346300265@qq.com>
2021-03-20 10:41:25 +0800

    add insure sdk

---
+++ finance/insure/sdk.go
@@ -0,0 +1,16 @@
+// author: wsfuyibing <websearch@163.com>
+// date: 2021-03-05
+
+// SDK for bill of finance.
+package insuresdk
+
+import "github.com/fuyibing/sdk"
+
+const (
+	Name = "jm-insure"
+)
+
+// Create bill.
+func GetByInsurerIdAndPolicyNo(ctx interface{}, body interface{}) *sdk.ClientResponse {
+	return sdk.NewHttp(Name).SetRoute("/policy/getByInsurerIdAndPolicyNo").SetBody(body).Run(ctx)
+}
