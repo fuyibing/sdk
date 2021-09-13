260e6cb7899f8185d8ae6ca75ad13dc5718cc654
Darrow <346300265@qq.com>
2021-09-13 18:14:15 +0800

    添加gsbillsdk

---
+++ finance/gsbillsdk/sdk.go
@@ -0,0 +1,16 @@
+// author: wsfuyibing <websearch@163.com>
+// date: 2021-03-05
+
+// SDK for bill of finance.
+package gsbillsdk
+
+import "github.com/fuyibing/sdk"
+
+const (
+	Name = "gs.bill	"
+)
+
+// Create bill.
+func BillPayoutBusinessApply(ctx interface{}, body interface{}) *sdk.ClientResponse {
+	return sdk.NewHttp(Name).SetRoute("/bill/payout/business/apply").SetBody(body).Run(ctx)
+}
