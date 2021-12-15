b416755f8926acf07f9e584f7f3cb52c4d3bb9ec
Frank Wang <frankwdy11@gmail.com>
2021-12-15 21:12:09 +0800

    应收开票sdk

--- finance/javabillsdk/sdk.go
+++ finance/javabillsdk/sdk.go
@@ -10,22 +10,22 @@ const (
 	Name = "js-fin-bill"
 )
 
-// 应付开票
+// PayoutBillApply 应付开票
 func PayoutBillApply(ctx interface{}, body interface{}) *sdk.ClientResponse {
 	return sdk.NewHttp(Name).SetRoute("/bill/payout/save").SetBody(body).Run(ctx)
 }
 
-// 应收开票
+// IncomeBillApply 应收开票
 func IncomeBillApply(ctx interface{}, body interface{}) *sdk.ClientResponse {
 	return sdk.NewHttp(Name).SetRoute("/bill/income/save").SetBody(body).Run(ctx)
 }
 
-// 根据 开票单业务单号 查询发票
+// InvoicePageByBusinessNo 根据 开票单业务单号 查询发票
 func InvoicePageByBusinessNo(ctx interface{}, body interface{}) *sdk.ClientResponse {
 	return sdk.NewHttp(Name).SetRoute("/invoice/pageByBusinessNo").SetBody(body).Run(ctx)
 }
 
-// 根据 开票单业务单号 查询发票最大的时间
+// InvoiceGetSignAndSendTime 根据 开票单业务单号 查询发票最大的时间
 func InvoiceGetSignAndSendTime(ctx interface{}, body interface{}) *sdk.ClientResponse {
 	return sdk.NewHttp(Name).SetRoute("/invoice/get/signAndSendTime").SetBody(body).Run(ctx)
 }
