30c37e957a422fb6bf771e232c4ce699eba92f7f
markwang1992 <617514823@qq.com>
2022-03-24 13:48:21 +0800

    gsrxmanagementsdk

---
+++ dpsp/gsrxmanagementsdk/sdk.go
@@ -0,0 +1,14 @@
+//@author: markwang <617514823@qq.com>
+//@date: 2022/3/24
+package gsrxmanagementsdk
+
+import "github.com/fuyibing/sdk"
+
+const (
+	Name = "gs-rx-management"
+)
+
+// 处方更新
+func RxUpdate(ctx interface{}, body interface{}) *sdk.ClientResponse {
+	return sdk.NewHttp(Name).SetRoute("/rx/update").SetBody(body).Run(ctx)
+}
