6f11a05c043ecfc3748462c4d25d5c77c9c0cdae
markwang <617514823@qq.com>
2022-04-01 17:50:39 +0800

    javambssdk

---
+++ dpsp/javambssdk/sdk.go
@@ -0,0 +1,15 @@
+// @Author markwang <wangyu@uniondrug.cn>
+// @Date   2022/4/1
+
+package javambssdk
+
+import "github.com/fuyibing/sdk"
+
+const (
+	Name = "java.mbs"
+)
+
+// 发布
+func topicPublish(ctx interface{}, body interface{}) *sdk.ClientResponse {
+	return sdk.NewHttp(Name).SetRoute("/topic/publish").SetBody(body).Run(ctx)
+}
