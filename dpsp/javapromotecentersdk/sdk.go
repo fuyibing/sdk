8c5995b2965a2d20522f10a92a0a5775b3dcfd8a
chenwei <chenwei@uniondrug.cn>
2022-04-24 18:45:46 +0800

    营销发卡 SDK

---
+++ dpsp/javapromotecentersdk/sdk.go
@@ -0,0 +1,12 @@
+package javambssdk
+
+import "github.com/fuyibing/sdk"
+
+const (
+	Name = "java.promotecenter.service"
+)
+
+// 发放卡
+func SendCard(ctx interface{}, body interface{}) *sdk.ClientResponse {
+	return sdk.NewHttp(Name).SetRoute("/card/sendCard").SetBody(body).Run(ctx)
+}
