3f7c7a52cfed996d2cdb39c3a95b0ce22efa7232
markwang <617514823@qq.com>
2022-05-09 11:05:44 +0800

    商户中心sdk

--- dpsp/merchantmodulesdk/sdk.go
+++ dpsp/merchantmodulesdk/sdk.go
@@ -12,3 +12,13 @@ const (
 func BackEndEditIsMember(ctx interface{}, body interface{}) *sdk.ClientResponse {
 	return sdk.NewHttp(Name).SetRoute("/organizebasebackend/editismember").SetBody(body).Run(ctx)
 }
+
+// 商户开通连锁小程序服务
+func BackEndEditIsAppletService(ctx interface{}, body interface{}) *sdk.ClientResponse {
+	return sdk.NewHttp(Name).SetRoute("/organizebasebackend/editIsAppletService").SetBody(body).Run(ctx)
+}
+
+// 商户详情
+func organizebaseInfo(ctx interface{}, body interface{}) *sdk.ClientResponse {
+	return sdk.NewHttp(Name).SetRoute("/organizebase/info").SetBody(body).Run(ctx)
+}
