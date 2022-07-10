9c575f37d995777ca630540d347c1cf19adf72aa
wang.dongyu <wangdongyu@uniondrug.com>
2022-07-10 11:44:27 +0800

    go sdk: outreach third party service

---
+++ outreach/thirdparty/sdk.go
@@ -0,0 +1,17 @@
+// author: wsfuyibing <websearch@163.com>
+// date: 2021-03-05
+
+// Package thirdparty SDK for outreach
+package thirdparty
+
+import (
+	"github.com/fuyibing/sdk"
+)
+
+const (
+	Name = "outreach-resource-api"
+)
+
+func CallThirdPartyService(ctx interface{}, body interface{}) *sdk.ClientResponse {
+	return sdk.NewHttp(Name).SetRoute("/api/adapter").SetBody(body).Run(ctx)
+}
