b8ffe62f7cec465169bdc9422c321c59f7b2d872
chen.wei <chenwei@uniondrug.com>
2022-08-01 11:48:52 +0800

    pmotoappointsdk

---
+++ dpsp/pmotoappointsdk/sdk.go
@@ -0,0 +1,12 @@
+package pmotoappointsdk
+
+import "github.com/fuyibing/sdk"
+
+const (
+	Name = "pm-oto-appoint"
+)
+
+// 小程序会员购买埋点
+func goBuried(ctx interface{}, body interface{}) *sdk.ClientResponse {
+	return sdk.NewHttp(Name).SetRoute("/order/goSensor").SetBody(body).Run(ctx)
+}
